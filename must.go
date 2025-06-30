90beaa912cc24354df8283f1f7e856ece9f2fd55
Andrew Hawker <andrew.r.hawker@gmail.com>
2025-06-30 14:47:44 -0700

    refactor: panic err

--- must.go
+++ must.go
@@ -5,9 +5,9 @@ import (
 	"reflect"
 )
 
-// ErrMustPanic is returned when a 'must' assertion fails and we must panic.
-var ErrMustPanic = errors.Known{
-	Code:      "must_panic",
+// ErrPanic is returned when a 'must' assertion fails and we must panic.
+var ErrPanic = errors.Defined{
+	Code:      "panic",
 	Message:   "assertion failed",
 	Namespace: "go-stdlib/go-must",
 }
@@ -16,15 +16,16 @@ var ErrMustPanic = errors.Known{
 func Alias[T any](v any) T {
 	t, ok := v.(T)
 	if !ok {
-		panic(ErrMustPanic.Wrapf("must.Alias[%s] cannot alias value to %T", name[T](), v))
+		panic(ErrPanic.Wrapf("must.Alias[%s](%T)", name[T](), v))
 	}
 	return t
 }
 
 // KnownErr panics if given error contains any unknown errors.
 func KnownErr(err error) {
-	if errors.As(err, errors.ErrUnknown) {
-		panic(ErrMustPanic.Wrapf("must.KnownErr has unknown %w", err))
+	var ke errors.Error
+	if !errors.As(err, &ke) {
+		panic(ErrPanic.Wrapf("must.KnownErr(%w)", err))
 	}
 }
 
@@ -33,28 +34,32 @@ func KnownErr(err error) {
 // This is equivalent to 'V0'.
 func NotErr(err error) {
 	if err != nil {
-		panic(ErrMustPanic.Wrapf("must.NotErr received error %v", err))
+		panic(ErrPanic.Wrapf("must.NotErr(%w)", err))
 	}
 }
 
 // NotZero panics if given value is equal to the zero value of the type.
 func NotZero[T any](t T) T {
 	if reflect.ValueOf(t).IsZero() {
-		panic(ErrMustPanic.Wrapf("must.NotZero[%s] received zero value", name[T]()))
+		panic(ErrPanic.Wrapf("must.NotZero[%s]", name[T]()))
 	}
 	return t
 }
 
 // Fn is an alias for 'Fn1' which is the most common case.
 func Fn[T any](fn func() (T, error)) T {
-	return Fn1(fn)
+	v, err := fn()
+	if err != nil {
+		panic(ErrPanic.Wrapf("must.Fn[%s] error=%w", name[T](), err))
+	}
+	return v
 }
 
 // Fn1 panics if the given function returns an error otherwise the value is returned.
 func Fn1[T any](fn func() (T, error)) T {
 	v, err := fn()
 	if err != nil {
-		panic(ErrMustPanic.Wrapf("must.Fn1[%s] returned error %w", name[T](), err))
+		panic(ErrPanic.Wrapf("must.Fn1[%s] error=%w", name[T](), err))
 	}
 	return v
 }
@@ -63,7 +68,7 @@ func Fn1[T any](fn func() (T, error)) T {
 func Fn2[T1 any, T2 any](fn func() (T1, T2, error)) (T1, T2) {
 	v1, v2, err := fn()
 	if err != nil {
-		panic(ErrMustPanic.Wrapf("must.Fn2[%s, %s] returned error %w", name[T1](), name[T2](), err))
+		panic(ErrPanic.Wrapf("must.Fn2[%s, %s] error=%w", name[T1](), name[T2](), err))
 	}
 	return v1, v2
 }
@@ -73,14 +78,14 @@ func Fn2[T1 any, T2 any](fn func() (T1, T2, error)) (T1, T2) {
 // This is equivalent to 'NotErr'.
 func V0(err error) {
 	if err != nil {
-		panic(ErrMustPanic.Wrapf("must.V0 received error %w", err))
+		panic(ErrPanic.Wrapf("must.V0 error=%w", err))
 	}
 }
 
 // V1 panics if given error is set otherwise the value is returned.
 func V1[T any](t T, err error) T {
 	if err != nil {
-		panic(ErrMustPanic.Wrapf("must.V1[%s] received error %w", name[T](), err))
+		panic(ErrPanic.Wrapf("must.V1[%s] error=%w", name[T](), err))
 	}
 	return t
 }
@@ -88,7 +93,7 @@ func V1[T any](t T, err error) T {
 // V2 panics if given error is set otherwise the value is returned.
 func V2[T1 any, T2 any](t1 T1, t2 T2, err error) (T1, T2) {
 	if err != nil {
-		panic(ErrMustPanic.Wrapf("must.V2[%s, %s] received error %w", name[T1](), name[T2](), err))
+		panic(ErrPanic.Wrapf("must.V2[%s, %s] error=%w", name[T1](), name[T2](), err))
 	}
 	return t1, t2
 }
